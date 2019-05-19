94320904e64a3c041e5ce3f53186454716aca11d
Roberto Barbosa <robertojf95@gmail.com>
2019-05-18 22:04:58 -0300

    create makeHeader function

--- projects/array-to-file-sheets/src/lib/array-to-file-sheets.service.ts
+++ projects/array-to-file-sheets/src/lib/array-to-file-sheets.service.ts
@@ -6,4 +6,31 @@ import { Injectable } from '@angular/core';
 export class ArrayToFileSheetsService {
 
   constructor() { }
+
+  headers: Array<string> = [];
+
+  /**
+    * @param array Array of datas
+    * @param fileName File name
+    * @param customHeader Headers
+  */
+  convert(array: Array<{ any }>, fileName: string, customHeader: any = true) {
+    let tableHeader = '<tr>';
+    let tableBody = '';
+  }
+
+
+  makeHeader(header: any, customHeader) {
+    let tableHeader = '';
+    let headerTitle = '';
+// tslint:disable-next-line: forin
+    for (const key in header) {
+      headerTitle = customHeader ? key : header[key];
+      tableHeader += `<td> ${headerTitle} </td>`;
+      this.headers.push(headerTitle);
+    }
+    return tableHeader;
+  }
+
+  
 }
