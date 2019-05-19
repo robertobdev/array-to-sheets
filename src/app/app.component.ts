3dd4ccc341f44b06a7eac6e78924119922b4ed78
Roberto Barbosa <robertojf95@gmail.com>
2019-05-18 22:51:39 -0300

    create example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ArrayToFileSheetsService } from 'projects/array-to-file-sheets/src/public-api';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'arrayToFileSheets';
+  constructor(private arrayToSheets : ArrayToFileSheetsService) { }
+  makeXls() {
+
+    const array: Array<any> = [
+      {
+        a: '11',
+        b: '22',
+        c: '33',
+        d: '44',
+      },
+      {
+        a: '11321',
+        b: '23212',
+        c: '356153',
+        d: '46864',
+      },
+      {
+        a: '21311',
+        b: '2512',
+        c: '32133',
+        d: '465214',
+      },
+      {
+        a: '1611',
+        b: '26122',
+        c: '41233',
+        d: '45214',
+      }
+    ];
+
+
+    this.arrayToSheets.convert(array, 'testee');
+  }
 }
